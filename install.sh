aea22ffc32d019e2a396d2c5e2ba32a11712d045
Nick Gerner <nick.gerner@gmail.com>
2011-03-30 11:47:39 -0700

    refactoring install.sh to link bins as well as configs so they are in a well known, common location

--- install.sh
+++ install.sh
@@ -1,34 +1,84 @@
 #!/bin/bash
 set -e
+#set -x
 
-sources=(config/.bashrc config/.vimrc config/.gitconfig config/.ctags)
-destinations=(~/.bashrc ~/.vimrc ~/.gitconfig ~/.ctags)
-count=${#sources[@]}
-if [ $count -ne ${#destinations[@]} ]
-then 
-	echo "UH OH, destinations and sources don't match length! quitting"
-	exit 1
+s1="config/.bashrc config/.vimrc config/.gitconfig config/.ctags"
+d1="$HOME/.bashrc $HOME/.vimrc $HOME/.gitconfig $HOME/.ctags"
+
+linkDestinationsToSources() {
+		if [[ -z $1 || -z $2 ]]
+		then
+			echo "need sources destinations! s: $1 d: $2"
+			exit 1
+		fi
+
+		sources=( `echo "$1"` )
+		destinations=( `echo "$2"` )
+		count=${#sources[@]}
+
+		echo "${sources[@]}"
+		echo "${destinations[@]}"
+		echo $count
+
+		if [ $count -ne ${#destinations[@]} ]
+		then 
+			echo "UH OH, destinations and sources don't match length! quitting"
+			echo ${sources[@]}
+			echo ${destinations[@]} 
+			echo $count ${#destinations[@]}
+			exit 1
+		fi
+
+		currDir=`pwd`
+		for ((i=0; i<count; i+=1))
+		do
+			s=$currDir/${sources[$i]}
+			if [ ! -e $s ]
+			then
+				echo "UH OH, no file $s in environment! skipping..."
+				continue
+			fi
+			d=${destinations[$i]}
+			echo -n "$d"
+			if [ -e $d ]
+			then 
+				if [ $d -ef $s ]
+				then echo " already linked to environment"
+				else echo " already exists! skipping. just rm it and run this again."
+				fi
+			else
+				ln -s $s $d
+				echo " linked from $d"
+			fi
+		done
+}
+
+#first link the configs
+echo "linking configs..."
+linkDestinationsToSources "$s1" "$d1"
+
+#then link the executables to ~/bin
+
+#make sure ~/bin exists
+if [[ ! -d ~/bin ]]
+then
+	if [[ -e ~/bin ]]
+	then
+		echo "UH OH, ~/bin exists, but isn't a directory!"
+		exit 1
+	else
+		echo "creating ~/bin..."
+		mkdir ~/bin
+	fi
 fi
 
-currDir=`pwd`
+s=`find bin/ -executable -type f`
+s_array=( `echo "$s"` )
+d=""
+count=${#s_array[@]}
 for ((i=0; i<count; i+=1))
 do
-	s=$currDir/${sources[$i]}
-	if [ ! -e $s ]
-	then
-		echo "UH OH, no file $s in environment! skipping..."
-		continue
-	fi
-	d=${destinations[$i]}
-	echo -n $d
-	if [ -e $d ]
-	then 
-		if [ $d -ef $s ]
-		then echo " already linked to environment"
-		else echo " already exists! skipping. just rm it and run this again."
-		fi
-	else
-		ln -s $s $d
-		echo " linked from $d"
-	fi
+	d="$d $HOME/${s_array[$i]}"
 done
+echo "linking binaries..."
+linkDestinationsToSources "$s" "$d"
